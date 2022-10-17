938daf09cdf896a7a93c8a5703c73033ddd82df1
little-15 <101408939+little-15@users.noreply.github.com>
2022-10-17 22:46:50 +0200

    feat: add vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,110 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+    this.alphabetLength = 26;
+    this.asciiA = 65;
+    this.asciiZ = 90;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let arrMessage = message.toUpperCase().split('');
+    while (key.length < message.length) {
+      key += key;
+    }
+
+    let arrFromKey = key.toUpperCase().split('');
+    let shiftNumKey = arrFromKey.map((item) => {
+      return item.charCodeAt() - this.asciiA;
+    });
+    let counter = 0;
+
+    let encryptMessage = arrMessage.map((item, index) => {
+      if (
+        item === ' ' ||
+        item.charCodeAt() > this.asciiZ ||
+        item.charCodeAt() < this.asciiA
+      ) {
+        counter++;
+        return item;
+      } else {
+        if (
+          item.charCodeAt() + shiftNumKey[index - counter] >
+          this.asciiZ
+        ) {
+          return String.fromCharCode(
+            item.charCodeAt() +
+              shiftNumKey[index - counter] -
+              this.alphabetLength
+          );
+        } else {
+          return String.fromCharCode(
+            item.charCodeAt() + shiftNumKey[index - counter]
+          );
+        }
+      }
+    });
+
+    if (this.mode === false) {
+      return encryptMessage.reverse().join('');
+    }
+    return encryptMessage.join('');
+  }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let arrMessage = message.split('');
+    while (key.length < message.length) {
+      key += key;
+    }
+
+    let arrFromKey = key.toUpperCase().split('');
+    let counter = 0;
+
+    let decryptMessage = arrMessage.map((item, index) => {
+      if (
+        item === ' ' ||
+        item.charCodeAt() > this.asciiZ ||
+        item.charCodeAt() < this.asciiA
+      ) {
+        counter++;
+        return item;
+      } else {
+        if (
+          item.charCodeAt() -
+            this.asciiA +
+            (this.asciiZ + 1 - arrFromKey[index - counter].charCodeAt()) >=
+          this.alphabetLength
+        ) {
+          return String.fromCodePoint(
+            item.charCodeAt() -
+              this.asciiA +
+              (this.asciiZ +
+                1 -
+                arrFromKey[index - counter].charCodeAt()) +
+              this.asciiA -
+              this.alphabetLength
+          );
+        } else {
+          return String.fromCodePoint(
+            item.charCodeAt() -
+              this.asciiA +
+              (this.asciiZ +
+                1 -
+                arrFromKey[index - counter].charCodeAt()) +
+              this.asciiA
+          );
+        }
+      }
+    });
+    
+    if (this.mode === false) {
+      return decryptMessage.reverse().join('');
+    }
+    return decryptMessage.join('');
   }
 }
 
